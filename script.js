1c91c0018885bb7026514fc96341fadc00ec0953
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2023-01-24 20:44:10 -0600

    feat: update background color when player wins

--- script.js
+++ script.js
@@ -2,39 +2,46 @@
 
 // Establish Secret Number
 const secretNumber = Math.trunc(Math.random() * 20 + 1);
-
-// Variables
-const btnCheck = document.querySelector(".btn_type_check");
-const numInput = document.querySelector(".game__number");
-const userMessage = document.querySelector(".game__message");
-const gameScore = document.querySelector(".game__score_span");
-let userScore = 20;
+let dynamicScore = 20;
 document.querySelector(".header__number").textContent = secretNumber;
 
+// Variables
+const body = document.querySelector("body");
+
+const btnCheck = document.querySelector(".btn_type_check");
+const gameNumberInput = document.querySelector(".game__number");
+const gameUserMessage = document.querySelector(".game__message");
+const gameUserScore = document.querySelector(".game__score_span");
+const gameScoreDisplay = document.querySelector(".header__number");
+
+function playerWins() {
+  body.classList.add("player-wins_background");
+  gameScoreDisplay.classList.add("player-wins_score");
+}
+
 function handleNumberCheck() {
-  const guess = Number(numInput.value);
+  const guess = Number(gameNumberInput.value);
 
-  userMessage.textContent = "🥳 Correct Number!";
-
-  if (userScore > 1) {
+  if (dynamicScore > 1) {
     if (!guess) {
-      userMessage.textContent = "C'mon! You didn't even guess :)";
+      gameUserMessage.textContent = "C'mon! You didn't even guess :)";
     } else if (guess === secretNumber) {
-      userMessage.textContent = "Correct Number!";
-      userScore++;
-      gameScore.textContent = userScore;
+      gameUserMessage.textContent = "Correct Number!";
+      dynamicScore++;
+      gameUserScore.textContent = dynamicScore;
+      playerWins();
     } else if (guess > secretNumber) {
-      userMessage.textContent = "A little high...try again!";
-      userScore--;
-      gameScore.textContent = userScore;
+      gameUserMessage.textContent = "A little high...try again!";
+      dynamicScore--;
+      gameUserScore.textContent = dynamicScore;
     } else {
-      userMessage.textContent = "A little low...try again!";
-      userScore--;
-      gameScore.textContent = userScore;
+      gameUserMessage.textContent = "A little low...try again!";
+      dynamicScore--;
+      gameUserScore.textContent = dynamicScore;
     }
   } else {
-    userMessage.textContent = "You lost!";
-    gameScore.textContent = 0;
+    gameUserMessage.textContent = "You lost!";
+    gameUserScore.textContent = 0;
   }
 }
 
