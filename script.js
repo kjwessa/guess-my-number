e6c62edaee023cfd04e3f5d7179449c13a6b0803
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2023-01-24 19:49:19 -0600

    feat: add user messages if they guess too high or low

--- script.js
+++ script.js
@@ -10,18 +10,28 @@
 
 // * Check the score
 
+// Establish Secret Number
+const secretNumber = Math.trunc(Math.random() * 10 + 1);
+
 // Variables
 const btnCheck = document.querySelector(".btn_type_check");
 const numInput = document.querySelector(".game__number");
 const userMessage = document.querySelector(".game__message");
+document.querySelector(".header__number").textContent = secretNumber;
 
 function handleNumberCheck() {
   const guess = Number(numInput.value);
-  console.log(guess);
-  document.querySelector(".game__message").textContent = "🥳 Correct Number!";
+
+  userMessage.textContent = "🥳 Correct Number!";
 
   if (!guess) {
     userMessage.textContent = "C'mon! You didn't even guess :)";
+  } else if (guess === secretNumber) {
+    userMessage.textContent = "Correct Number!";
+  } else if (guess > secretNumber) {
+    userMessage.textContent = "A little high...try again!";
+  } else {
+    userMessage.textContent = "A little low...try again!";
   }
 }
 
