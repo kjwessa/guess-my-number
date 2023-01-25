0e070f67c69d5ac1252889a999d0e171e081bedd
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2023-01-24 19:34:00 -0600

    feat: update user message is number input is submitted empty

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-'use strict';
+"use strict";
+
+// document.querySelector(".game__message").textContent = "🥳 Correct Number!";
+
+// document.querySelector(".header__number").textContent = 6;
+
+// document.querySelector(".game__score_span").textContent = 10;
+
+// document.querySelector(".game__number").value = 4;
+
+// * Check the score
+
+// Variables
+const btnCheck = document.querySelector(".btn_type_check");
+const numInput = document.querySelector(".game__number");
+const userMessage = document.querySelector(".game__message");
+
+function handleNumberCheck() {
+  const guess = Number(numInput.value);
+  console.log(guess);
+  document.querySelector(".game__message").textContent = "🥳 Correct Number!";
+
+  if (!guess) {
+    userMessage.textContent = "C'mon! You didn't even guess :)";
+  }
+}
+
+btnCheck.addEventListener("click", handleNumberCheck);
