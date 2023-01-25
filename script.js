d12bb7b39806a5ae12832049caf28cbf76695d16
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2023-01-24 20:07:45 -0600

    feat: add message when the user loses the game

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 "use strict";
 
 // Establish Secret Number
-const secretNumber = Math.trunc(Math.random() * 10 + 1);
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 // Variables
 const btnCheck = document.querySelector(".btn_type_check");
 const numInput = document.querySelector(".game__number");
 const userMessage = document.querySelector(".game__message");
 const gameScore = document.querySelector(".game__score_span");
-let userScore = 10;
+let userScore = 20;
 document.querySelector(".header__number").textContent = secretNumber;
 
 function handleNumberCheck() {
@@ -16,20 +16,25 @@ function handleNumberCheck() {
 
   userMessage.textContent = "🥳 Correct Number!";
 
-  if (!guess) {
-    userMessage.textContent = "C'mon! You didn't even guess :)";
-  } else if (guess === secretNumber) {
-    userMessage.textContent = "Correct Number!";
-    userScore++;
-    gameScore.textContent = userScore;
-  } else if (guess > secretNumber) {
-    userMessage.textContent = "A little high...try again!";
-    userScore--;
-    gameScore.textContent = userScore;
+  if (userScore > 1) {
+    if (!guess) {
+      userMessage.textContent = "C'mon! You didn't even guess :)";
+    } else if (guess === secretNumber) {
+      userMessage.textContent = "Correct Number!";
+      userScore++;
+      gameScore.textContent = userScore;
+    } else if (guess > secretNumber) {
+      userMessage.textContent = "A little high...try again!";
+      userScore--;
+      gameScore.textContent = userScore;
+    } else {
+      userMessage.textContent = "A little low...try again!";
+      userScore--;
+      gameScore.textContent = userScore;
+    }
   } else {
-    userMessage.textContent = "A little low...try again!";
-    userScore--;
-    gameScore.textContent = userScore;
+    userMessage.textContent = "You lost!";
+    gameScore.textContent = 0;
   }
 }
 
